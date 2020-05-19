c82155f7711c5c7bf2d0378f86e3c3a072e92d1f
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-19 17:43:10 +0900

    버튼 생성, 동적 생성

--- FirstFormApplication/Form1.Designer.cs
+++ FirstFormApplication/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.myButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myButton
+            // 
+            this.myButton.Location = new System.Drawing.Point(31, 28);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(75, 23);
+            this.myButton.TabIndex = 0;
+            this.myButton.Text = "오예은";
+            this.myButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button myButton;
     }
 }
 
--- FirstFormApplication/Form1.cs
+++ FirstFormApplication/Form1.cs
@@ -15,6 +15,19 @@ namespace FirstFormApplication
         public Form1()
         {
             InitializeComponent();
+            myButton.Enabled = true;
+            myButton.Text = "코드에서 변경~";
+            myButton.Width = 200;
+
+            for(int i = 0; i<5; i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+
+                button.Location = new Point(313, 13 + (23 + 3)*i);
+                button.Text = "동적생성" + (i+1) +"번째";
+                button.Width = 100;
+            }
         }
     }
 }
